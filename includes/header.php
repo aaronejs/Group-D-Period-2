f35346000f4fb99604b2993d232e6da1a07e1bd6
TheLobster1 <rjeveldman@hotmail.nl>
2021-11-24 15:10:13 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="./css/main.css">
     <link rel="stylesheet" href="./css/login.css">
 </head>
 <body>
