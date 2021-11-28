7bed8e9f51c1e303645ae4a1d4a48a50f4aab970
TheLobster1 <rjeveldman@hotmail.nl>
2021-11-28 21:44:28 +0100

    main page search bar
    Added image, did the thing

--- index.php
+++ index.php
@@ -11,6 +11,30 @@
     ?>
     <main>
 
+<?php 
+$date = date("Y-m-d");
+$time = date("H:i");
+?>
+
+      <form>
+        <div class="searchBox">
+          <div class="boxItem">
+            <h3>Date</h3>
+            <input type="date" name="searchDate" value="<?php echo $date;?>">
+          </div>
+          <div class="boxItem">
+            <h3>Start time</h3>
+            <input type="time" name="startTime" value="<?php echo $time;?>">
+          </div>
+          <div class="boxItem">
+            <h3>End time</h3>
+            <input type="time" name="endTime" value="<?php echo $time;?>">
+          </div>
+          <button class="searchButton" type="submit" name="submit">
+            <img src="./resources/search.png" id="searchImg">
+          </button>
+        </div>
+      </form>
     </main>
     <?php
     include './includes/footer.html'; // footer
