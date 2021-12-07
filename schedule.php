5d2cadb5a00e3fce1a5728c2b2746a3eb21fc479
Aaron Eljas <aaron.eljas@student.nhlstenden.com>
2021-12-07 21:47:35 +0100

    Fixed schedule
    Schedule has fixed height and content is scrollable

--- schedule.php
+++ schedule.php
@@ -14,95 +14,102 @@
           <div class="tableWeek">
             <h1>WEEK 1</h1>
           </div>
-          <table>
-            <tr>
-              <th>SUNDAY</th>
-              <th>MONDAY</th>
-              <th>TUESDAY</th>
-              <th>WEDNESDAY</th>
-              <th>THURSDAY</th>
-              <th>FRIDAY</th>
-              <th>SATURDAY</th>
-            </tr>
-            <tr>
-              <td>
-                <h2>0.231</h2>
-                <p>IVAN IVANOV</p>
-                <p2>12:30 - 19:45</p2>
-              </td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              <td>6</td>
-              <td>7</td>
-            </tr>
-            <tr>
-              <td>
-                <h2>0.231</h2>
-                <p>IVAN IVANOV</p>
-                <p2>12:30 - 19:45</p2>
-              </td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              <td>6</td>
-              <td>7</td>
-            </tr>
-            <tr>
-              <td>
-                <h2>0.231</h2>
-                <p>IVAN IVANOV</p>
-                <p2>12:30 - 19:45</p2>
-              </td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              <td>6</td>
-              <td>7</td>
-            </tr>
-            <tr>
-              <td>
-                <h2>0.231</h2>
-                <p>IVAN IVANOV</p>
-                <p2>12:30 - 19:45</p2>
-              </td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              <td>6</td>
-              <td>7</td>
-            </tr>
-            <tr>
-              <td>
-                <h2>0.231</h2>
-                <p>IVAN IVANOV</p>
-                <p2>12:30 - 19:45</p2>
-              </td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              <td>6</td>
-              <td>7</td>
-            </tr>
-            <tr>
-              <td>
-                <h2>0.231</h2>
-                <p>IVAN IVANOV</p>
-                <p2>12:30 - 19:45</p2>
-              </td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              <td>6</td>
-              <td>7</td>
-            </tr>
-          </table>
+          <div class="tableHeader">
+            <table>
+              <tr>
+                <th>SUNDAY</th>
+                <th>MONDAY</th>
+                <th>TUESDAY</th>
+                <th>WEDNESDAY</th>
+                <th>THURSDAY</th>
+                <th>FRIDAY</th>
+                <th>SATURDAY</th>
+              </tr>
+            </table>
+          </div>
+          <div class="tableContent">
+            <table>
+              <tr>
+                <td>
+                  <h2>0.231</h2>
+                  <p>IVAN IVANOV</p>
+                  <p2>12:30 - 19:45</p2>
+                </td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td>7</td>
+              </tr>
+              <tr>
+                <td>
+                  <h2>0.231</h2>
+                  <p>IVAN IVANOV</p>
+                  <p2>12:30 - 19:45</p2>
+                </td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td>7</td>
+              </tr>
+              <tr>
+                <td>
+                  <h2>0.231</h2>
+                  <p>IVAN IVANOV</p>
+                  <p2>12:30 - 19:45</p2>
+                </td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td>7</td>
+              </tr>
+              <tr>
+                <td>
+                  <h2>0.231</h2>
+                  <p>IVAN IVANOV</p>
+                  <p2>12:30 - 19:45</p2>
+                </td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td>7</td>
+              </tr>
+              <tr>
+                <td>
+                  <h2>0.231</h2>
+                  <p>IVAN IVANOV</p>
+                  <p2>12:30 - 19:45</p2>
+                </td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td>7</td>
+              </tr>
+              <tr>
+                <td>
+                  <h2>0.231</h2>
+                  <p>IVAN IVANOV</p>
+                  <p2>12:30 - 19:45</p2>
+                </td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td>7</td>
+              </tr>
+            </table>
+          </div>
+
         </div>
       </main>
     <?php
