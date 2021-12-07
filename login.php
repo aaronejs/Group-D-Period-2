e73064d738b2040fdc8674b22c645da449edb6b3
Aaron Eljas <aaron.eljas@student.nhlstenden.com>
2021-12-07 19:56:58 +0100

    Updated login
    Added sessionId to check if user is logged in on other pages

--- login.php
+++ login.php
@@ -46,7 +46,7 @@
                       <input type="submit" name="login" value="Log in">
                     </div>
                     <div class="center">
-                      <p>Don't have an account?</p> <a href="#">Sign up here!</a>
+                      <p>Don't have an account?</p> <a href="./register.php">Sign up here!</a>
                     </div>
                   </form>
                 </div>
@@ -59,21 +59,32 @@
                     echo "<div class='success'>".$success."</div>";
                   }
                 }
+                if(isset($_GET['error'])){
+                  if($_GET['error'] == "login"){
+                    echo "<div class='warning'>Please log in to use the service!</div>";
+                  }
+                }
                 if (isset($_POST['login'])) {
                   if(!empty($_POST['email']) && !empty($_POST['password'])){
                     if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){ //validate email
                       $email = $_POST['email'];
-                      $sql = "SELECT first_name, last_name, password FROM user WHERE email = ?"; //query to insert into database
+                      $sql = "SELECT id, first_name, last_name, password, verified FROM user WHERE email = ?"; //query to insert into database
                       if($stmt = mysqli_prepare($conn, $sql)){ //database parses, compiles, and performs query optimization and stores w/o executing
                         mysqli_stmt_bind_param($stmt, "s", $email); //need to bind values to parameters
                         if(mysqli_stmt_execute($stmt)){ //execute the statement
-                          mysqli_stmt_bind_result($stmt, $firstname, $lastname, $password); //bind results
+                          mysqli_stmt_bind_result($stmt, $id, $firstname, $lastname, $password, $verified); //bind results
                           mysqli_stmt_store_result($stmt);
                           if(mysqli_stmt_num_rows($stmt) != 0){
                             while(mysqli_stmt_fetch($stmt)){
                               if (password_verify($_POST['password'], $password)) { //verify password
-                                $_SESSION['firstname'] = $firstname; //set session variables to use across pages
-                                $_SESSION['lastname'] = $lastname;
+                                if($verified == 1){
+                                  $_SESSION['firstname'] = $firstname; //set session variables to use across pages
+                                  $_SESSION['lastname'] = $lastname;
+                                  $_SESSION['sessionID'] = $id;
+                                  header("location:./index.php?success=login");
+                                }else{
+                                  $error = "Account not verified!";
+                                }
                               } else {
                                 $error = "Incorrect password!";
                               }
