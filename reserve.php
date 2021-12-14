3a173070bc2af61d076ab2deed7f0581a68b570c
Aaron Eljas <aaron.eljas@student.nhlstenden.com>
2021-12-14 19:09:14 +0100

    Added dynamic room availability

--- reserve.php
+++ reserve.php
@@ -2,7 +2,8 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="main.css">
+		<link rel="stylesheet" type="text/css" href="./css/main.css">
+		<link rel="stylesheet" href="./css/room.css">
 		<title>Reservation</title>
 	</head>
 	<body>
@@ -40,10 +41,33 @@
 					}else{
 						$error = "Error: " . mysqli_error($conn);
 					}
-
+					$availability1=$availability2=$availability3=$availability4=$availability5=$availability6=$availability7 = "roomSelect";
 					if(mysqli_stmt_num_rows($stmt) != 0){
 						while(mysqli_stmt_fetch($stmt)){
-							echo $id;
+							$array = str_split($room_id);
+							foreach ($array as $key) {
+								if($key == 1){
+									$availability1 = "roomNoSelect";
+								}
+								elseif($key == 2){
+									$availability2 = "roomNoSelect";
+								}
+								elseif($key == 3){
+									$availability3 = "roomNoSelect";
+								}
+								elseif($key == 4){
+									$availability4 = "roomNoSelect";
+								}
+								elseif($key == 5){
+									$availability5 = "roomNoSelect";
+								}
+								elseif($key == 6){
+									$availability6 = "roomNoSelect";
+								}
+								elseif($key == 7){
+									$availability7 = "roomNoSelect";
+								}
+							}
 						}
 					}else{
 						$error = "No bookings!";
@@ -56,23 +80,131 @@
 				<?= $error ?>
 				<?= $error2 ?>
 				<div class="mainItem">
-					<img src="./resources/map.png" alt="Map" class="reserveMap">
-					<div class="formReserve">
-						<form method="post" class="form">
-							<p><label for="numberOfPeople">Number Of People</label></p>
-							<p><label for="laptop">Laptop</label>
-							<input type="checkbox" name="laptop" class="reserveCheckbox"></p>
-							<p><label for="extensionCabel">Extension Cabel</label>
-							<input type="checkbox" name="extensionCabel" class="reserveCheckbox"></p>
-							<p><label for="tv">TV</label>
-							<input type="checkbox" name="tv" class="reserveCheckbox"></p>
-							<p><label for="option">Option</label>
-							<input type="checkbox" name="option" class="reserveCheckbox"></p>
-							<p><label for="option">Option</label>
-							<input type="checkbox" name="option" class="reserveCheckbox"></p>
-							<input type="submit" name="confirm" value="Confirm" class="reserveButton">
-						</form>
-					</div>
+					<div class="room-content">
+	            <div class="select-room">
+	                <svg width="719" height="374" viewBox="0 0 719 374" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+	                        <rect x="0.716064" y="2.43384" width="717" height="371" transform="rotate(-0.116409 0.716064 2.43384)" fill="url(#pattern0)"/>
+													<rect x="555" y="342" width="48" height="22" transform="rotate(-0.116409 544 58.2764)"  fill-opacity="0.56" id="room_1" class="<?=$availability1?>"/>
+													<rect x="439" y="304" width="79" height="59" transform="rotate(-0.116409 48.8297 58.3362)" fill-opacity="0.56" id="room_2" class="<?=$availability2?>"/>
+													<rect x="439" y="256" width="79" height="45" transform="rotate(-0.116409 48.8297 58.3362)" fill-opacity="0.56" id="room_3" class="<?=$availability3?>"/>
+													<rect x="301" y="256" width="134" height="57" transform="rotate(-0.116409 48.8297 58.3362)" fill-opacity="0.56" id="room_4" class="<?=$availability4?>"/>
+													<rect x="113" y="222" width="92" height="91" transform="rotate(-0.116409 48.8297 58.3362)" fill-opacity="0.56" id="room_5" class="<?=$availability5?>"/>
+	                        <rect x="113" y="132" width="93" height="86" transform="rotate(-0.116409 417.829 57.5865)"  fill-opacity="0.68" id="room_6" class="<?=$availability6?>"/>
+	                        <rect x="402" y="20" width="124" height="85" transform="rotate(-0.116409 417.829 57.5865)"  fill-opacity="0.68" id="room_7" class="<?=$availability7?>"/>
+	                        <defs>
+	                        <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
+	                        <use xlink:href="#image0_531_1277" transform="translate(0 -0.000409126) scale(0.000892857 0.00172555)"/>
+	                        </pattern>
+	                    </defs>
+	                   </svg>
+	                <img src="./resources/room.svg" alt="" class="bg-room">
+	            </div>
+	            <div class="room-params" id="params" style="display: none;">
+	                <div class="tabs-btns">
+	                    <div class="tab-btn active" id="btn1">Room</div>
+	                    <div class="tab-btn" id="btn2">Equipment</div>
+	                </div>
+	                <div class="tabs">
+	                    <div class="tab" id="room">
+	                        <span>Number of people</span>
+	                        <form method="post">
+														<input type="number" id="quantity" name="quantity" min="1" max="24"><br><br>
+														<input type="submit" name="submit" value="Confirm">
+	                        </form>
+	                    </div>
+	                    <div class="tab" id="equipment" style="display:none">
+	                        <div class="select-equipment">
+	                            <span>Select equipment</span>
+	                            <div class="item">
+	                                <div class="checkbox">
+	                                    <input id="Laptop" type="checkbox" name="check" value="check1">
+	                                    <label for="Laptop"></label>
+	                                </div>
+	                                <span>Laptop</span>
+	                                <div class="count" id="0">
+	                                    <div class="minus">-</div>
+	                                    <div class="number">0</div>
+	                                    <div class="plus">+</div>
+	                                </div>
+	                            </div>
+	                            <div class="item">
+	                                <div class="checkbox">
+	                                    <input id="Extension" type="checkbox" name="check" value="check1">
+	                                    <label for="Extension"></label>
+	                                </div>
+	                                <span>Extension cable</span>
+	                                <div class="count" id="1">
+	                                    <div class="minus">-</div>
+	                                    <div class="number">0</div>
+	                                    <div class="plus">+</div>
+	                                </div>
+	                            </div>
+	                            <div class="item">
+	                                <div class="checkbox">
+	                                    <input id="TV" type="checkbox" name="check" value="check1">
+	                                    <label for="TV"></label>
+	                                </div>
+	                                <span>TV</span>
+	                                <div class="count" id="2">
+	                                    <div class="minus">-</div>
+	                                    <div class="number">0</div>
+	                                    <div class="plus">+</div>
+	                                </div>
+	                            </div>
+	                            <div class="item">
+	                                <div class="checkbox">
+	                                    <input id="Teapot" type="checkbox" name="check" value="check1">
+	                                    <label for="Teapot"></label>
+	                                </div>
+	                                <span>Teapot</span>
+	                                <div class="count" id="3">
+	                                    <div class="minus">-</div>
+	                                    <div class="number">0</div>
+	                                    <div class="plus">+</div>
+	                                </div>
+	                            </div>
+	                            <div class="item">
+	                                <div class="checkbox">
+	                                    <input id="Adapter" type="checkbox" name="check" value="check1">
+	                                    <label for="Adapter"></label>
+	                                </div>
+	                                <span>Adapter</span>
+	                                <div class="count" id="4">
+	                                    <div class="minus">-</div>
+	                                    <div class="number">0</div>
+	                                    <div class="plus">+</div>
+	                                </div>
+	                            </div>
+	                            <div class="item">
+	                                <div class="checkbox">
+	                                    <input id="Adapter2" type="checkbox" name="check" value="check1">
+	                                    <label for="Adapter2"></label>
+	                                </div>
+	                                <span>Adapter</span>
+	                                <div class="count" id="5">
+	                                    <div class="minus">-</div>
+	                                    <div class="number">0</div>
+	                                    <div class="plus">+</div>
+	                                </div>
+	                            </div>
+	                            <div class="item">
+	                                <div class="checkbox">
+	                                    <input id="Adapter3" type="checkbox" name="check" value="check1">
+	                                    <label for="Adapter3"></label>
+	                                </div>
+	                                <span>Adapter</span>
+	                                <div class="count" id="6">
+	                                    <div class="minus">-</div>
+	                                    <div class="number">0</div>
+	                                    <div class="plus">+</div>
+	                                </div>
+	                            </div>
+	                        </div>
+	                    </div>
+
+	                </div>
+	            </div>
+	        </div>
 				</div>
 				<div class="reserveText">
 					<img src="./resources/ReservePic.png" alt="Thinking person illustration" class="reservePicture">
@@ -87,5 +219,6 @@
         <?php
             include './includes/footer.html'; // Footer
         ?>
+				<script src="room.js"></script>
 	</body>
 </html>
