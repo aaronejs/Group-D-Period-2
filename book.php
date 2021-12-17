57e08b64dd38ee185f3db9b2971a1c38598c1009
TheLobster1 <rjeveldman@hotmail.nl>
2021-12-17 18:49:22 +0100

    updates
    made book.php show the error
    added more text to index.php
    adjusted some css

--- book.php
+++ book.php
@@ -16,7 +16,7 @@ if(isset($_POST['submit'])){
         mysqli_stmt_bind_param($stmt, "ssssss", $_SESSION['sessionID'], $item, $room_id, $startTime, $endTime, $date); //bind the param to be the email from the form
         if(!mysqli_stmt_execute($stmt)){ //execute the statement
           $error = "Error executing query" . mysqli_error($conn);
-          die(); //die if we cant execute statement
+          die($error); //die if we cant execute statement
         }else{
           header("location:./reserve.php?success=booked");
         }
--- index.php
+++ index.php
@@ -17,15 +17,15 @@
         <div class="mainItem">
           <div class="mainItemText">
             <h1>Making reservations</h1>
-            <p>Making reservations can be done by using the search bar located above. Here you will enter the amount of people that you need. There will be more information in this paragraph when the website will be built. This is just text to fill up the paragraph so it looks like a similar size to when it will go live.</p>
+            <p>Making reservations can be done by using the search bar located above. Here you can enter at what time you want to reserve a room. When you press the search button, you will be sent to a page where you can select which room and/or items you want to reserve for that timeframe.</p>
           </div>
           <img src="./resources/Working.png" alt="People working">
         </div>
         <div class="mainItem">
-          <img src="./resources/Working.png">
+          <img src="./resources/Traveling.png">
           <div class="mainItemText">
-            <h1>Making reservations</h1>
-            <p>Making reservations can be done by using the search bar located above. Here you will enter the amount of people that you need. There will be more information in this paragraph when the website will be built. This is just text to fill up the paragraph so it looks like a similar size to when it will go live.</p>
+            <h1>Discover our events</h1>
+            <p>Would you like to experience what it’s like to study in the Netherlands at NHL Stenden University of Applied Sciences? Meet our students and academic staff during our online and offine events! </br><a href="https://www.nhlstenden.com/en/events" target="_blank"><button class="squareButton">Events</button></a></p>
           </div>
         </div>
         <div class="mainItem">
