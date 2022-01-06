1f79fdfb70f71fb548ba8b49e14a4ad29fc52060
Nyximus32 <miroslav.penchev@student.nhlstenden.com>
2022-01-06 15:12:37 +0200

    Schedule updates
    looks less scuffed;
    schedule now only displays bookings for the current week;
    working on being able to change weeks

--- schedule.php
+++ schedule.php
@@ -15,141 +15,165 @@
   ?>
   <div class="schedule">
     <div class="week">
-      <h1>Week <?php echo idate('W',strtotime(date("Y/m/d"))) ?></h1>
+      <h1>Week <?php  $currentweek=idate('W',strtotime(date("Y/m/d")));
+                      echo $currentweek;
+                ?>
+      </h1>
     </div>
-    <div class="monday">
-      <p>monday</p>
-    </div>
-    <div class="tuesday">
-      <p>tuesday</p>
-    </div>
-    <div class="wednesday">
-      <p>wednesday</p>
-    </div>
-    <div class="thursday">
-      <p>thursday</p>
-    </div>
-    <div class="friday">
-      <p>friday</p>
-    </div>
-    <div class="saturday">
-      <p>saturday</p>
-    </div>
-    <div class="sunday">
-      <p>sunday</p>
-    </div>
+    <div class="days">
+      <div class="monday day">
+        <p>Monday</p>
+      </div>
+      <div class="tuesday day">
+        <p>Tuesday</p>
+      </div>
+      <div class="wednesday day">
+        <p>Wednesday</p>
+      </div>
+      <div class="thursday day">
+        <p>Thursday</p>
+      </div>
+      <div class="friday day">
+        <p>Friday</p>
+      </div>
+      <div class="saturday day">
+        <p>Saturday</p>
+      </div>
+      <div class="sunday day">
+        <p>Sunday</p>
+      </div>
+      <?php
 
-    <?php
+        /*function dayAssigner($fName,$lName,$roomNr,$startTime,$endTime){    It's gonna work at some point, trust
+          echo'
+                <h5>'.$fName.' '.$lName.'</h5>
+                <p>Room:'.$roomNr.'</p>
+                <p>Time:'.$startTime.'-'.$endTime.'</p>
+              ';
+        }*/
 
-      /*function dayAssigner($fName,$lName,$roomNr,$startTime,$endTime){    It's gonna work at some point, trust
-        echo'
-              <h5>'.$fName.' '.$lName.'</h5>
-              <p>Room:'.$roomNr.'</p>
-              <p>Time:'.$startTime.'-'.$endTime.'</p>
-            ';
-      }*/
+        include './includes/database.php';
+        $sql="SELECT U.first_name, U.last_name, R.room_nr, R.floor_nr, B.start_time, B.end_time, B.date
+              FROM booking B
+              JOIN room R ON B.room_id=R.id
+              JOIN user U ON B.user_id=U.id
+              ORDER BY B.start_time;";
 
-      include './includes/database.php';
-      $sql="SELECT U.first_name, U.last_name, R.room_nr, R.floor_nr, B.start_time, B.end_time, B.date
-            FROM booking B
-            JOIN room R ON B.room_id=R.id
-            JOIN user U ON B.user_id=U.id
-            ORDER BY B.start_time;";
+        if($stmt = mysqli_prepare($conn, $sql)){
+          mysqli_stmt_execute($stmt);
+          mysqli_stmt_bind_result($stmt,$fName,$lName,$roomNr,$floorNr,$startTime,$endTime,$date);
+          mysqli_stmt_store_result($stmt);
+          if(mysqli_stmt_num_rows($stmt) != 0){
+            while(mysqli_stmt_fetch($stmt)){
+              $dayofweek = date('l', strtotime($date));
+              $weekofyear = idate('W',strtotime($date));
 
-      if($stmt = mysqli_prepare($conn, $sql)){
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_bind_result($stmt,$fName,$lName,$roomNr,$floorNr,$startTime,$endTime,$date);
-        mysqli_stmt_store_result($stmt);
-        if(mysqli_stmt_num_rows($stmt) != 0){
-          while(mysqli_stmt_fetch($stmt)){
-            $dayofweek = date('l', strtotime($date));
-            $weekofyear = idate('W',strtotime($date));
-            $zeros = strlen($roomNr);
-            if($zeros == 1) {
-                $zero = "00";
-            }
-            elseif($zeros == 2) {
-                $zero = "0";
-            }
-            else{
-                $zero = "";
-            }
-            if ($dayofweek=="Monday") {
-              echo'
-              <div class="monday">
-              <h4>'.$fName.' '.$lName.'</h4>
-              <p>Room: '.$floorNr.'.'.$zero.$roomNr.'</p>
-              <p>Time: '.$startTime.' - '.$endTime.'</p> 
-              </div>
-              ';
-            }
-            if ($dayofweek=="Tuesday") {
-              echo'
-              <div class="tuesday">
-              <h4>'.$fName.' '.$lName.'</h4>
-              <p>Room: '.$floorNr.'.'.$zero.$roomNr.'</p>
-              <p>Time: '.$startTime.' - '.$endTime.'</p> 
-              </div>
-              ';
-            }
-            if ($dayofweek=="Wednesday") {
-              echo'
-              <div class="wednesday">
-              <h4>'.$fName.' '.$lName.'</h4>
-              <p>Room: '.$floorNr.'.'.$zero.$roomNr.'</p>
-              <p>Time: '.$startTime.' - '.$endTime.'</p> 
-              </div>
-              ';
-            }
-            if ($dayofweek=="Thursday") {
-              echo'
-              <div class="thursday">
-              <h4>'.$fName.' '.$lName.'</h4>
-              <p>Room: '.$floorNr.'.'.$zero.$roomNr.'</p>
-              <p>Time: '.$startTime.' - '.$endTime.'</p> 
-              </div>
-              ';
-            }
-            if ($dayofweek=="Friday") {
-              echo'
-              <div class="friday">
-              <h4>'.$fName.' '.$lName.'</h4>
-              <p>Room: '.$floorNr.'.'.$zero.$roomNr.'</p>
-              <p>Time: '.$startTime.' - '.$endTime.'</p> 
-              </div>
-              ';
-            }
-            if ($dayofweek=="Saturday") {
-              echo'
-              <div class="saturday">
-              <h4>'.$fName.' '.$lName.'</h4>
-              <p>Room: '.$floorNr.'.'.$zero.$roomNr.'</p>
-              <p>Time: '.$startTime.' - '.$endTime.'</p> 
-              </div>
-              ';
-            }
-            if ($dayofweek=="Sunday") {
-              echo'
-              <div class="sunday">
-              <h4>'.$fName.' '.$lName.'</h4>
-              <p>Room: '.$floorNr.'.'.$zero.$roomNr.'</p>
-              <p>Time: '.$startTime.' - '.$endTime.'</p> 
-              </div>
-              ';
+              if ($weekofyear==$currentweek) {
+                $zeros = strlen($roomNr);
+                if($zeros == 1) {
+                    $zero = "00";
+                }
+                elseif($zeros == 2) {
+                    $zero = "0";
+                }
+                else{
+                    $zero = "";
+                }
+                if ($dayofweek=="Monday") {
+                  echo'
+                  <div class="monday day">
+                  <h4>'.$fName.' '.$lName.'</h4>
+                  <p>Room: '.$floorNr.'.'.$zero.$roomNr.'</p>
+                  <p>Time: '.$startTime.' - '.$endTime.'</p> 
+                  </div>
+                  ';
+                }
+                if ($dayofweek=="Tuesday") {
+                  echo'
+                  <div class="tuesday day">
+                  <h4>'.$fName.' '.$lName.'</h4>
+                  <p>Room: '.$floorNr.'.'.$zero.$roomNr.'</p>
+                  <p>Time: '.$startTime.' - '.$endTime.'</p> 
+                  </div>
+                  ';
+                }
+                if ($dayofweek=="Wednesday") {
+                  echo'
+                  <div class="wednesday day">
+                  <h4>'.$fName.' '.$lName.'</h4>
+                  <p>Room: '.$floorNr.'.'.$zero.$roomNr.'</p>
+                  <p>Time: '.$startTime.' - '.$endTime.'</p> 
+                  </div>
+                  ';
+                }
+                if ($dayofweek=="Thursday") {
+                  echo'
+                  <div class="thursday day">
+                  <h4>'.$fName.' '.$lName.'</h4>
+                  <p>Room: '.$floorNr.'.'.$zero.$roomNr.'</p>
+                  <p>Time: '.$startTime.' - '.$endTime.'</p> 
+                  </div>
+                  ';
+                }
+                if ($dayofweek=="Friday") {
+                  echo'
+                  <div class="friday day">
+                  <h4>'.$fName.' '.$lName.'</h4>
+                  <p>Room: '.$floorNr.'.'.$zero.$roomNr.'</p>
+                  <p>Time: '.$startTime.' - '.$endTime.'</p> 
+                  </div>
+                  ';
+                }
+                if ($dayofweek=="Saturday") {
+                  echo'
+                  <div class="saturday day">
+                  <h4>'.$fName.' '.$lName.'</h4>
+                  <p>Room: '.$floorNr.'.'.$zero.$roomNr.'</p>
+                  <p>Time: '.$startTime.' - '.$endTime.'</p> 
+                  </div>
+                  ';
+                }
+                if ($dayofweek=="Sunday") {
+                  echo'
+                  <div class="sunday day">
+                  <h4>'.$fName.' '.$lName.'</h4>
+                  <p>Room: '.$floorNr.'.'.$zero.$roomNr.'</p>
+                  <p>Time: '.$startTime.' - '.$endTime.'</p> 
+                  </div>
+                  ';
+                }
+              }
             }
           }
+          else{
+              echo "nothing to show";
+            }
         }
         else{
-            echo "nothing to show";
-          }
-      }
-      else{
-        echo"beep boop db error";
-      }
-    ?>
+          echo"beep boop db error";
+        }
+
+        /*echo '';
+        if(isset($_POST['nxt'])){
+          $currentweek+=1;
+          echo $currentweek;
+        }
+        if(isset($_POST['pvs'])){
+          $currentweek-=1;
+          echo $currentweek;
+        }*/
+
+        
+
+      ?>
+    </div>
+      <form class="move" action="schedule.php" method="post">
+                <input type="submit" name="nxt" value="Last week"><br><br>
+                <input type="submit" name="pvs" value="Next week">
+      </form>
   </div>
-  <?php
-  include './includes/footer.html'; // footer
-  ?>
+    <?php
+    include './includes/footer.html'; // footer
+    ?>
 </body>
 </html>
