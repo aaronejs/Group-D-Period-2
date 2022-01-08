9da0b3c532464b41904095d1f1e1fb01aa4dbee3
TheLobster1 <rjeveldman@hotmail.nl>
2022-01-08 19:28:11 +0100

    css fixes
    Fixed the css.

--- index.php
+++ index.php
@@ -25,25 +25,25 @@
 			include './includes/search.php';
 			?>
         <div class="mainItem">
-          <div class="mainItemText">
+          <div class="mainItemText mainItemContent">
             <h1>Making reservations</h1>
             <p>Making reservations can be done by using the search bar located above. Here you can enter at what time you want to reserve a room. When you press the search button, you will be sent to a page where you can select which room and/or items you want to reserve for that timeframe.</p>
           </div>
-          <img src="./resources/Working.png" alt="Generic image">
+          <img src="./resources/Working.png" alt="Generic image" class="mainItemContent mainItemImage">
         </div>
         <div class="mainItem">
-          <img src="./resources/Traveling.png" alt="Generic image">
-          <div class="mainItemText">
+          <img src="./resources/Traveling.png" alt="Generic image" class="mainItemContent mainItemImage">
+          <div class="mainItemText mainItemContent">
             <h1>Discover our events</h1>
             <p>Would you like to experience what it’s like to study in the Netherlands at NHL Stenden University of Applied Sciences? Meet our students and academic staff during our online and offine events! </br><a href="https://www.nhlstenden.com/en/events" target="_blank"><button class="smallSquareButton">Events</button></a></p>
           </div>
         </div>
         <div class="mainItem">
-          <div class="mainItemText">
+          <div class="mainItemText mainItemContent">
             <h1>Making reservations</h1>
             <p>Making reservations can be done by using the search bar located above. Here you will enter the amount of people that you need. There will be more information in this paragraph when the website will be built. This is just text to fill up the paragraph so it looks like a similar size to when it will go live.</p>
           </div>
-          <img src="./resources/Relaxing.png" alt="Generic image">
+          <img src="./resources/Relaxing.png" alt="Generic image" class="mainItemContent mainItemImage">
         </div>
       </div>
     </main>
