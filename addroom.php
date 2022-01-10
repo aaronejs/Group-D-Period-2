2817e68b902497b903950afbfafe7254d99cec52
Jadooooon <91316558+Jadout7@users.noreply.github.com>
2022-01-10 14:22:14 +0400

    Update addroom.php

--- addroom.php
+++ addroom.php
@@ -45,4 +45,28 @@
                 </div>
             </div>          
         </main>
-    </body>
+        <?php
+        include './includes/database.php';
+        session_start();
+        
+        if(isset($_POST['createroom'])){
+          if(!empty($_POST['roomnum']) && !empty($_POST['floornum']) && !empty($_POST['cap'])){
+            $Room_nr = $_GET['roomnum'];
+            $Floor_nr = $_GET['floornum'];
+            $Capacity = $_GET['cap'];
+            $sql = "INSERT INTO Item (user_id, $Room_nr, $Floor_nr, $Capacity)
+                    VALUES (?,?,?,?)";
+            if($stmt = mysqli_prepare($conn, $sql)){ //database parses, compiles, and performs query optimization and stores w/o executing
+            mysqli_stmt_bind_param($stmt, "ssss", $_SESSION['sessionID'], $Room_nr, $Floor_nr, $Capacity); //bind the param to be the email from the form
+            if(!mysqli_stmt_execute($stmt)){ //execute the statement
+              $error = "Error executing query" . mysqli_error($conn);
+              die($error); //die if we cant execute statement
+              }
+            }
+          }
+        }
+        ?>
+        <?php
+        include './includes/footer.html'; // footer
+        ?>
+        </body>
