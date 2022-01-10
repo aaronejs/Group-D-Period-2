3fa88804969515d5e9f0b2dc474106e381678467
Jadooooon <91316558+Jadout7@users.noreply.github.com>
2022-01-10 14:24:03 +0400

    Add files via upload

---
+++ additem.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<?php include './includes/head.html'; ?>
+<link href="./css/login.css" rel="stylesheet" type="text/css">
+<title>Maintanance Page</title>
+</head>
+<body>    
+    <?php
+    include './includes/header.php'; // header
+    ?>
+        <main>
+            <div class="center">
+                <div class="formBox">
+                    <div class="contentText">
+                    <h1>Add an Item</h1>
+                    </div>
+                    <div class="form">
+                        <form method="post">
+                        <div>
+                            <fieldset>
+                            <legend>Item Name</legend>
+                            <input type="text" name="Iname">
+                            </fieldset>
+                        </div>
+    </body> 
