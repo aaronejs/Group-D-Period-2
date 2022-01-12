d7328ae512437fa330ff38f26e1470bee416e4c8
Aaron Eljas <aaron.eljas@student.nhlstenden.com>
2022-01-12 10:00:49 +0100

    redirect to 2FA

--- login.php
+++ login.php
@@ -70,38 +70,50 @@
                   if(!empty($_POST['email']) && !empty($_POST['password'])){
                     if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){ //validate email
                       $email = $_POST['email'];
-                      $sql = "SELECT id, first_name, last_name, password, verified, user_type FROM user WHERE email = ?"; //query to insert into database
+                      $sql = "SELECT id, first_name, last_name, password, 2FA, verified, user_type FROM user WHERE email = ?"; //query to insert into database
                       if($stmt = mysqli_prepare($conn, $sql)){ //database parses, compiles, and performs query optimization and stores w/o executing
                         mysqli_stmt_bind_param($stmt, "s", $email); //need to bind values to parameters
                         if(mysqli_stmt_execute($stmt)){ //execute the statement
-                          mysqli_stmt_bind_result($stmt, $id, $firstname, $lastname, $password, $verified, $type); //bind results
+                          mysqli_stmt_bind_result($stmt, $id, $firstname, $lastname, $password, $twoFactor, $verified, $type); //bind results
                           mysqli_stmt_store_result($stmt);
                           if(mysqli_stmt_num_rows($stmt) != 0){
                             while(mysqli_stmt_fetch($stmt)){
                               if (password_verify($_POST['password'], $password)) { //verify password
                                 if($verified == 1){
-                                  $_SESSION['firstname'] = $firstname; //set session variables to use across pages
-                                  $_SESSION['lastname'] = $lastname;
-                                  $_SESSION['sessionID'] = $id;
-                                  $_SESSION['email'] = $email;
-                                  $_SESSION['type'] = $type;
-                                  if(isset($_POST['rememberLogin'])){
-                                    $sql = "UPDATE user set token = ? WHERE id = ?";
-                                    if($stmt = mysqli_prepare($conn, $sql)){
-                                      $token = time().$id;
-                                      mysqli_stmt_bind_param($stmt, "ss", $token, $id);
-                                      if(!mysqli_stmt_execute($stmt)){ //execute the statement
-                          							$error = "Error executing query" . mysqli_error($conn);
-                          							die();
-                                      }else{
-                                        $token = password_hash($token, PASSWORD_DEFAULT);
-                                        $hour = time() + 3600 * 24 * 30;
-                                        setcookie('userid', $id, $hour);
-                                        setcookie('token', $token, $hour);
+                                  if($twoFactor == 0){
+                                    $_SESSION['firstname'] = $firstname; //set session variables to use across pages
+                                    $_SESSION['lastname'] = $lastname;
+                                    $_SESSION['sessionID'] = $id;
+                                    $_SESSION['email'] = $email;
+                                    $_SESSION['type'] = $type;
+                                    if(isset($_POST['rememberLogin'])){
+                                      $sql = "UPDATE user set token = ? WHERE id = ?";
+                                      if($stmt = mysqli_prepare($conn, $sql)){
+                                        $token = time().$id;
+                                        mysqli_stmt_bind_param($stmt, "ss", $token, $id);
+                                        if(!mysqli_stmt_execute($stmt)){ //execute the statement
+                                          $error = "Error executing query" . mysqli_error($conn);
+                                          die();
+                                        }else{
+                                          $token = password_hash($token, PASSWORD_DEFAULT);
+                                          $hour = time() + 3600 * 24 * 30;
+                                          setcookie('userid', $id, $hour);
+                                          setcookie('token', $token, $hour);
+                                        }
                                       }
                                     }
+                                    header("location:./index.php?success=login");
+                                  }else{
+                                    $_SESSION['temp_firstname'] = $firstname; //set session variables to use across pages
+                                    $_SESSION['temp_lastname'] = $lastname;
+                                    $_SESSION['temp_sessionID'] = $id;
+                                    $_SESSION['temp_email'] = $email;
+                                    $_SESSION['temp_type'] = $type;
+                                    if(isset($_POST['rememberLogin'])){
+                                      $_SESSION['rememberLogin'];
+                                    }
+                                    header('location: ./2FA.php');
                                   }
-                                  header("location:./index.php?success=login");
                                 }else{
                                   $error = "Account not verified!";
                                 }
