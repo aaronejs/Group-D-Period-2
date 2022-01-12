19a8f7b9ad818a2d4ca233f9075485cea0d35795
Aaron Eljas <aaron.eljas@student.nhlstenden.com>
2022-01-12 10:50:19 +0100

    Update 2FA.php
    big update, works!!!!!!

--- 2FA.php
+++ 2FA.php
@@ -11,49 +11,54 @@
     <?php
     include './includes/header.php'; // header
 
+    if(!isset($_SESSION['token_factor'])){
+      $mrand = rand(1000, 9999);
+      $_SESSION['token_factor'] = $mrand;
+      
+      $to      = $_SESSION['temp_email'];
+      $subject = '2FA code';
+      $message = $_SESSION['token_factor'];
+      $header = "From: test.nhlstenden@gmail.com \r\n";
+      $header .= "MIME-Version: 1.0" . "\r\n";
+      $header .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-    $mrand = rand(1000, 9999);
-    $_SESSION['token_factor'] = $mrand;
-    var_dump($_SESSION['token_factor']);
-    $to      = $_SESSION['temp_email'];
-    $subject = '2FA code';
-    $message = $_SESSION['token_factor'];
-    $header = "From: test.nhlstenden@gmail.com \r\n";
-    $header .= "MIME-Version: 1.0" . "\r\n";
-    $header .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-
-    if(!mail($to, $subject, $message, $header)){
-      session_unset();
-      session_destroy();
-      header('location: ./index.php?error=2fa');
+      if(!mail($to, $subject, $message, $header)){
+        session_unset();
+        session_destroy();
+        header('location: ./index.php?error=2fa');
+      }
     }
-
     if (isset($_POST['login'])) {
       if($_POST['code'] == $_SESSION['token_factor']) {
+        $firstname = $_SESSION['temp_firstname']; //set session variables to use across pages
+        $lastname = $_SESSION['temp_lastname'];
+        $id = $_SESSION['temp_sessionID'];
+        $emal = $_SESSION['temp_email'];
+        $type = $_SESSION['temp_type'];
         if($_SESSION['rememberLogin']){
-          if(isset($_POST['rememberLogin'])){
-            $sql = "UPDATE user set token = ? WHERE id = ?";
-            if($stmt = mysqli_prepare($conn, $sql)){
-              $token = time().$id;
-              mysqli_stmt_bind_param($stmt, "ss", $token, $id);
-              if(!mysqli_stmt_execute($stmt)){ //execute the statement
-                $error = "Error executing query" . mysqli_error($conn);
-                die();
-              }else{
-                $token = password_hash($token, PASSWORD_DEFAULT);
-                $hour = time() + 3600 * 24 * 30;
-                setcookie('userid', $id, $hour);
-                setcookie('token', $token, $hour);
-                session_unset();
-                $_SESSION['firstname'] = $firstname; //set session variables to use across pages
-                $_SESSION['lastname'] = $lastname;
-                $_SESSION['sessionID'] = $id;
-                $_SESSION['email'] = $email;
-                $_SESSION['type'] = $type;
-                header("location:./index.php?success=login");
-              }
+          $sql = "UPDATE user set token = ? WHERE id = ?";
+          if($stmt = mysqli_prepare($conn, $sql)){
+            $token = time().$id;
+            mysqli_stmt_bind_param($stmt, "ss", $token, $_SESSION['temp_sessionID']);
+            if(!mysqli_stmt_execute($stmt)){ //execute the statement
+              $error = "Error executing query" . mysqli_error($conn);
+              die();
+            }else{
+              $token = password_hash($token, PASSWORD_DEFAULT);
+              $hour = time() + 3600 * 24 * 30;
+              setcookie('userid', $id, $hour);
+              setcookie('token', $token, $hour);
+              session_unset();
+              $_SESSION['firstname'] = $firstname; //set session variables to use across pages
+              $_SESSION['lastname'] = $lastname;
+              $_SESSION['sessionID'] = $id;
+              $_SESSION['email'] = $email;
+              $_SESSION['type'] = $type;
+              header("location:./index.php?success=login");
             }
+          }else{
+            echo "broken";
           }
         }else{
           session_unset();
@@ -65,7 +70,7 @@
           header("location:./index.php?success=login");
         }
       } else {
-        var_dump(11155);
+        session_unset();
         header("location:./login.php?error=2FA");
       }
     }
