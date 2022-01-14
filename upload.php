64732770f99728d015afba5758142e898a7af9e4
Stef0002 <stefani.margaritova@student.nhlstenden.com>
2022-01-14 22:48:08 +0100

    more fixes on upload

--- upload.php
+++ upload.php
@@ -5,32 +5,32 @@
         <title>Upload Profile pic</title>
     </head>
     <body>
-    <?php
-        $uploadedFileType = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $_FILES["uploadedFile"]["tmp_name"]);
-        $acceptedFileTypes = ["image/jpg", "image/jpeg", "image/png"];
-        $fileinfo = @getimagesize($_FILES["uploadedFile"]["tmp_name"]);
-        $width = $fileinfo[0];
-        $height = $fileinfo[1];
+        <?php
+            $uploadedFileType = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $_FILES["uploadedFile"]["tmp_name"]);
+            $acceptedFileTypes = ["image/jpg", "image/jpeg", "image/png"];
+            $fileinfo = @getimagesize($_FILES["uploadedFile"]["tmp_name"]);
+            $width = $fileinfo[0];
+            $height = $fileinfo[1];
 
-        if(in_array($uploadedFileType, $acceptedFileTypes)) {
-            if ($_FILES["uploadedFile"]["error"] > 0) {
-                echo "Error: " . $_FILES["uploadedFile"]["error"] . "<br />";
-            }elseif ($_FILES["uploadedFile"]["size"] > 20000000) {
-                echo "Your file is too large.";
-            }elseif (strlen($_FILES["uploadedFile"]["name"])<=50000000) {
-                echo "File name is too large";
-            }elseif ($width > "800" && $height > "800") {
-                echo "Image dimension should be within 800X800."; 
-            }elseif (file_exists("./resources/profile_pictures/" . $_FILES["uploadedFile"]["name"])){
-                echo $_FILES["uploadedFile"]["name"] . " already exists. ";
-            }elseif(move_uploaded_file($_FILES["uploadedFile"]["tmp_name"], "./resources/profile_pictures/". $_FILES["uploadedFile"]["tmp_name"])){
-                echo "Profile photo uploaded";
+            if(in_array($uploadedFileType, $acceptedFileTypes)) {
+                if ($_FILES["uploadedFile"]["error"] > 0) {
+                    echo "Error: " . $_FILES["uploadedFile"]["error"] . "<br />";
+                }elseif ($_FILES["uploadedFile"]["size"] > 20000000) {
+                    echo "Your file is too large.";
+                }elseif (strlen($_FILES["uploadedFile"]["name"])>=20) {
+                    echo "File name is too large";
+                }elseif ($width > "800" && $height > "800") {
+                    echo "Image dimension should be within 800X800."; 
+                }elseif (file_exists("./resources/profilePictures/" . $_FILES["uploadedFile"]["name"])){
+                    echo $_FILES["uploadedFile"]["name"] . " already exists. ";
+                }elseif(move_uploaded_file($_FILES["uploadedFile"]["tmp_name"], "./resources/profilePictures/". $_FILES["uploadedFile"]["tmp_name"])){
+                    echo "Profile photo uploaded";
+                }else{
+                    echo "Something went wrong while uploading.";
+                }
             }else{
-                echo "Something went wrong while uploading.";
+                echo "Invalid file type. Must be png, jpg or jpeg.";
             }
-        }else{
-            echo "Invalid file type. Must be png, jpg or jpeg.";
-        }
-    ?>
+        ?>
     </body>
 </html>
