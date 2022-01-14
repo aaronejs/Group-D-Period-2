5a13f91833a73290b1e6ef19acf3d1e7dcc9d120
Stef0002 <stefani.margaritova@student.nhlstenden.com>
2022-01-14 22:43:54 +0100

    Fixes to upload

--- upload.php
+++ upload.php
@@ -6,24 +6,24 @@
     </head>
     <body>
     <?php
-        $uploadedFileType = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $_FILES["uploadedFile"]["profile_pic"]);
+        $uploadedFileType = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $_FILES["uploadedFile"]["tmp_name"]);
         $acceptedFileTypes = ["image/jpg", "image/jpeg", "image/png"];
-        $fileinfo = @getimagesize($_FILES["uploadedFile"]["profile_pic"]);
+        $fileinfo = @getimagesize($_FILES["uploadedFile"]["tmp_name"]);
         $width = $fileinfo[0];
         $height = $fileinfo[1];
 
         if(in_array($uploadedFileType, $acceptedFileTypes)) {
             if ($_FILES["uploadedFile"]["error"] > 0) {
                 echo "Error: " . $_FILES["uploadedFile"]["error"] . "<br />";
-            }elseif ($_FILES["uploadedFile"]["size"] > 200000) {
+            }elseif ($_FILES["uploadedFile"]["size"] > 20000000) {
                 echo "Your file is too large.";
-            }elseif (strlen($_FILES["uploadedFile"]["name"])<=20) {
+            }elseif (strlen($_FILES["uploadedFile"]["name"])<=50000000) {
                 echo "File name is too large";
-            }elseif ($width > "800" || $height > "800") {
+            }elseif ($width > "800" && $height > "800") {
                 echo "Image dimension should be within 800X800."; 
-            }elseif (file_exists("resources/" . $_FILES["uploadedFile"]["name"])){
+            }elseif (file_exists("./resources/profile_pictures/" . $_FILES["uploadedFile"]["name"])){
                 echo $_FILES["uploadedFile"]["name"] . " already exists. ";
-            }elseif(move_uploaded_file($_FILES["uploadedFile"]["profile_pic"], "resources/profile_pictures/". $_FILES["uploadedFile"]["profile_pic"])){
+            }elseif(move_uploaded_file($_FILES["uploadedFile"]["tmp_name"], "./resources/profile_pictures/". $_FILES["uploadedFile"]["tmp_name"])){
                 echo "Profile photo uploaded";
             }else{
                 echo "Something went wrong while uploading.";
